cd759af628bfe762baa56ce4f83472fdb915b090
Yqaqos <61677362+Yqaqos@users.noreply.github.com>
2024-06-29 00:49:46 -0700

    sevanbtchho0000000000000003e

--- chessbot/website/static/js/home.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-	var image = document.getElementById("active-game-image");
-
-	function update_active_game() {
-		image.src = image.src.split("?")[0] + "?" + new Date().getTime();
-	}
-
-	setInterval(update_active_game, 10000);
-}
--- run_website.py
+++
@@ -1,4 +0,0 @@
-from chessbot.config import *
-from chessbot.website import app
-
-app.run("localhost", port=WEBHOOK_PORT, debug=True)
